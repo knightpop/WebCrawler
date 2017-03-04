4b807f2781d840c43aa535895e7b004550f09671
ktz <knightpop@gmail.com>
2017-03-05 05:10:09 +0900

    first Commit of Study

--- ImageSpider.py
+++ ImageSpider.py
@@ -2,7 +2,8 @@ import scrapy
 
 class ImageSpider(scrapy.Spider):
     name = 'imageSpider'
-    start_url = ['']
+    # start_url = ['']
+    start_urls = ['http://web.humoruniv.com/board/humor/read.html?table=wphoto&number=104474']
 
     def parse(self, response):
-        pass
+        print(response.TextResponse.xpath('//img'))
