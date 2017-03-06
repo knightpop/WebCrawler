799575c01de206908ec1db79e207aa7f7c2b1931
ktz <knightpop@gmail.com>
2017-03-07 01:37:10 +0900

    first Commit of Study

--- ImageSpider.py
+++ ImageSpider.py
@@ -7,3 +7,4 @@ class ImageSpider(scrapy.Spider):
 
     def parse(self, response):
         print(response.TextResponse.xpath('//img'))
+## TODO response.css('div#wrap_img a::attr(href)').extract()이렇게 하면 되지 않을까?
